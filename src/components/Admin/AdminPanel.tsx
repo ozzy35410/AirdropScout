f18a4946f7d6299f4fda17e848798816edc246f2
Oguzhan Oguz <gsoguzhanoguz@gmail.com>
2025-10-04 17:02:45 +0200

    chore: prune old vite client

--- src/components/Admin/AdminPanel.tsx
+++
@@ -1,9 +0,0 @@
-import { ConfigAdminPanel } from './ConfigAdminPanel';
-
-interface AdminPanelProps {
-  language: 'en' | 'tr';
-}
-
-export function AdminPanel({ language }: AdminPanelProps) {
-  return <ConfigAdminPanel language={language} />;
-}
--- src/config/rpc.ts
+++
@@ -1,9 +0,0 @@
-// Public RPC endpoints for each supported network
-export const RPC_ENDPOINTS = {
-  base: 'https://mainnet.base.org',
-  sei: 'https://evm-rpc.sei-apis.com',
-  giwa: 'https://sepolia-rpc.giwa.io',
-  pharos: 'https://pharos-testnet.calderachain.xyz/http'
-} as const;
-
-export type NetworkKey = keyof typeof RPC_ENDPOINTS;
